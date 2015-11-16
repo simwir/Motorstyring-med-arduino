90224cb62117df81812e1e4e9a0236b1495f5613
simwir <simwir@hotmail.com>
2015-11-16 12:48:04 +0100

    Added logic for intervals

--- motorstyring/motorstyring.ino
+++ motorstyring/motorstyring.ino
@@ -3,7 +3,10 @@ int potAIn = 1; //Analog Read pin for the potentiometer
 int directionDOut = 12; //Digital write pin for the motor direction
 int speedPwmOut = 11; //Digital PWM write pin for the speed of the motor
 int debugPin = 13;
+
 double waitCurve = -16000/1023; //The gradient for the wait curve. -16000/1023 = between 20 and 4 sek
+double maxWait = 20000; //The maximum wait in millis
+unsigned long waitBegin; //The begin time of the wait in millis
 
 void setup() {
   //Setting the pinmodes for the pins
@@ -17,17 +20,17 @@ void loop() {
   //A switch depending on the state of the state switch
   switch(getState()){
     case 1:
-
-    break;
+      //Does nothing since this means off
+      break;
     case 2:
-
-    break;
+      modeWait();
+      break;
     case 3:
-
-    break;
+      slowMove();
+      break;
     case 4:
-
-    break;
+      fastMove();
+      break;
   }
 }
 /**
@@ -37,6 +40,7 @@ void loop() {
 int getSate(){
   int state = analogRead(stateAIn);
   if(state>884){
+    beginWait();
     return 1;
   }else if(state<=884&&state>628){
     return 2;
@@ -45,8 +49,40 @@ int getSate(){
   }else if(state<=383){
     return 3;
   }else{
+    //Should never happen, if it does something went very wrong.
     digitalWrite(debugPin, HIGH);
     return 1;
   }
 }
 
+/**
+ * Sets the wait timer to the current time
+ */
+void beginWait(){
+  waitBegin = millis();
+}
+
+/**
+ * Checks if the current tima has exceeded the time to wait.
+ * If that is true then is calls a slowMove() and beginWait()
+ */
+void modeWait(){
+  if(millis()>waitCurve*analogRead(1)+maxWait){
+    slowMove();
+    beginWait();
+  }
+}
+
+/**
+ * Moves the wiper slowly up and back a single time
+ */
+void slowMove(){
+  
+}
+
+/**
+ * Moves the wiper fast up and back a single time
+ */
+void fastMove(){
+  
+}
